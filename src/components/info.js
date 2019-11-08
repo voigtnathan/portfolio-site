25d35ee0eb8a1ba88bfef96d9bef3316ed8bb36c
Student Loaner <studentloaner@MacBook-Pro-4.local>
2019-11-08 09:40:05 -0600

    formatting change

--- src/components/info.js
+++ src/components/info.js
@@ -14,12 +14,6 @@ export default function info() {
         <span style = {{color : "white"}}>Hello, I'm</span> <br />
         Nate Voigt.
       </div>
-      <div className="h1 code mt-4 mb-3">aboutMe = {"{"}</div>
-      <div className="text-muted mx-5 my-4 h3 text-justify info">
-       description: A Passionate Full Stack Software Engineer, Backend developer, and Digital Strategist.
-        <br /><br /> howWorks: Solving the world's problems with coffee in one hand while typing with the other.
-      </div>
-      <div className="h1 code mt-2 mb-3">{"}"}</div>
       <div className="h1 mt-5">
         
         <a className="mr-5 icon" href="https://github.com/voigtnathan">
@@ -32,6 +26,13 @@ export default function info() {
           <FaLinkedin/>
         </a>
       </div>
+      <div className="h1 code mt-4 mb-3">aboutMe = {"{"}</div>
+      <div className="text-muted mx-5 my-4 h3 text-justify info">
+      <p>Nate Voigt: A software engineer with experience in full stack software engineering, digital marketing, and SEO & pagespeed optimization. 
+      Nate's love of problem-solving has shaped his path and he enjoys being challenged by his work. Continuously looking for new challenges has lead Nate to join a couple different development teams over the last 4 years and develop several mobile first web applications. 
+      When he's not working Nate enjoys traveling to new places and strong Italian espresso. </p></div>
+      <div className="h1 code mt-2 mb-3">{"}"}</div>
+     
     </div>
   )
 }

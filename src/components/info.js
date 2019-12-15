d4fe913247abb60baf83a0fb9788559cd98f72be
Nathaniel Eugene Hawthorne <Nate@Kyles-MacBook-Pro.local>
2019-12-15 10:27:10 -0600

    edited bio and added skills

--- src/components/info.js
+++ src/components/info.js
@@ -28,9 +28,14 @@ export default function info() {
       </div>
       <div className="h1 code mt-4 mb-3">aboutMe = {"{"}</div>
       <div className="text-muted mx-5 my-4 h3 text-justify info">
-      <p>Nate Voigt: A software engineer with experience in full stack software engineering, digital marketing, and SEO & pagespeed optimization. 
-      Nate's love of problem-solving has shaped his path and he enjoys being challenged by his work. Continuously looking for new challenges has lead Nate to join a couple different development teams over the last 4 years and develop several mobile first web applications. 
-      When he's not working Nate enjoys traveling to new places and strong Italian espresso. </p></div>
+      <p>Nate Voigt:    A software engineer with experience in full stack software engineering and digital strategy. 
+        I have worked on multiple apps of varying scale and function; from responsive portfolios to community-driven mobile first web applications. 
+        I believe in streamlined development that focuses on scalability, clean coding practices, and intuitive user experiences.  </p>
+        <p>Skills:   [ HTML, Jest Testing, CSS, Node.Js, jQuery, Git, 
+                        Express.js, React.js, Ember.js, Handlebars, Redux,
+                        Redux Sagas, Liquid, Material-UI, Semantic-UI, JavaScript,
+                        Axios, PostgreSQL, SQL, Java, Maven ]
+</p></div>
       <div className="h1 code mt-2 mb-3">{"}"}</div>
      
     </div>

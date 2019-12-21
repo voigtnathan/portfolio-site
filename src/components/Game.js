9d6220a347d145acc2d4746ce78341e45dc2a37e
Nate Voigt <voigtnathan23@gmail.com>
2019-12-21 11:30:33 -0600

    initial board state working and adding correct random digits to random tiles

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Game extends Component{
-    
+class Game extends Component {
+
 
     //inital state for game
     state = {
-        gameBoard: null,
+        gameBoard: [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ], //change to null when done testing
         score: 0,
         gameOver: false,
         message: null
@@ -13,48 +18,98 @@ class Game extends Component{
 
     //create starting board with two random coordinates with starting numbers
     createBoard = () => {
+
         let board = [
-            [0,0,0,0],
-            [0,0,0,0],
-            [0,0,0,0],
-            [0,0,0,0]
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
         ];
+
+        board = this.placeNewRandom(this.placeNewRandom(board));
+
         this.setState({
-            gameBoard:board
+            gameBoard: board,
+            score: 0,
+            gameOver: false,
+            message: null
         });
     }
-    
-    addStarters = () => {
-        //check for all zero spots
-        let options = [];
-        for(let i = 0; i < 4; i++){
-            for(let j = 0; j < 4; j++){
-                if(this.state.gameBoard[i][j] === 0){
-                    options.push({
-                        x: i,
-                        y: j
-                    })
+
+
+    //check for all zero spots on the board array
+    getAllBlankSpaces = (board) => {
+        let blankSpaces = [];
+        for (let r = 0; r < board.length; r++) {
+            for (let c = 0; c < board[r].length; c++) {
+                if (this.state.gameBoard[r][c] === 0) {
+                    blankSpaces.push([r, c])
                 }
             }
         }
+        return blankSpaces;
     }
-    //addStarters
+
+
+    //generate two random starting numbers either  or 4
+    createStartNumbers = () => {
+        const starters = [2, 4];
+        const randomNum = starters[Math.floor(Math.random() * starters.length)];
+
+        console.log(randomNum);
+
+        return randomNum;
+    }
+
+
     //function should pick two random tiles and add either 2 or 4 there
-    //DO AFTER FINDING BLANK SPACES ON BOARD
+    placeNewRandom = (board) => {
+        const blankTiles = this.getAllBlankSpaces(board);
+        let randomTile = blankTiles[Math.floor(Math.random() * blankTiles.length)];
+
+        console.log('random tile ' + randomTile);
+
+        let randomStarter = this.createStartNumbers();
+
+        board[randomTile[0]][randomTile[1]] = randomStarter;
+
+        console.log(board);
+
+        return board;
+    }
+
+    componentDidMount() {
+        this.createBoard();
+    }
 
 
-    
-
-
-
-
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <table>{this.state.gameBoard}</table>
+                <div id="demos">
+                    <button className='social' onClick={() => this.createBoard()}>NEW GAME</button>
+                </div>
+                <div id="demos">
+                    <table>
+                        <tbody>
+                            {
+                                this.state.gameBoard.map((row, i) => (
+                                    <tr key={i}>
+                                        {
+                                            row.map((tile, j) =>
+                                                <td className='tile' key={j}>{tile}</td>
+                                            )
+                                        }
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
+
 }
 
 export default Game;
